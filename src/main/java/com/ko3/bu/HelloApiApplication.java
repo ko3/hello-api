4f4be4941ba7f08217e236177f55b444c2577c06
ko3 <ek.lft15@gmail.com>
2019-07-01 17:25:03 -0500

    Initial commit

---
+++ src/main/java/com/ko3/bu/HelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.ko3.bu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApiApplication.class, args);
+	}
+
+}
